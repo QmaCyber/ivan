dffd9c27e2ef528cd3f97b529d66b6e12ed00562
Alen <49744385+Cpio1@users.noreply.github.com>
2025-12-05 11:35:04 +0500

    Add more names to the list in page-f97c4b30b39a8ed5.js

--- _next/static/chunks/app/page-f97c4b30b39a8ed5.js
+++ _next/static/chunks/app/page-f97c4b30b39a8ed5.js
@@ -7,7 +7,7 @@
 			"use strict";a.d(t, {
 				w: () => r
 			});
-			let s = ["Ivan Ivanov","Tomiris Almukhanbetova", "Omirzak Assyl", "Sabina Kozhagaliyeva", "Ekaterina Serazeeva", "Assylzhan Yelemes", "Adelya Kabdulda",];
+			let s = ["Ivan Ivanov","Tomiris Almukhanbetova", "Omirzak Assyl", "Sabina Kozhagaliyeva", "Ekaterina Serazeeva", "Assylzhan Yelemes", "Adelya Kabdulda", "Alisher Abylkassymov", "Yerassyl Zhagalbayev", "Aituar Serikbayev",];
 
 			function r(e, t) {
 				if(!e || !t) return !1;
