49df1c2a75985f0cd8e867d0636c12f0135f20ba
Alen <49744385+Cpio1@users.noreply.github.com>
2026-03-13 20:11:33 +0500

    Update page-691286c07902a63a.js

--- _next/static/chunks/app/start/page-691286c07902a63a.js
+++ _next/static/chunks/app/start/page-691286c07902a63a.js
@@ -4,7 +4,7 @@
 			"use strict";r.d(t, {
 				w: () => s
 			});
-			let n = ["Ivan Ivanov", "Alen Alymzhan"];
+			let n = ["Ivan Ivanov", "Arsen Zholaman", "Valensiya Makridina", "Ali Beisembek", "Meirkhan Kairbekov", "Rustam Yersainov"];
 
 			function s(e, t) {
 				if(!e || !t) return !1;
